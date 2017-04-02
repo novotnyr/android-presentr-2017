c5d85e1fc22550d222fbf6eb3670b4e17f9d7c67
Robert Novotny <robert.novotny@dcoresystems.com>
2017-04-02 11:52:27 +0200

    Pridana entita pre pouzivatela

---
+++ app/src/main/java/sk/upjs/ics/presentr2017/User.java
@@ -0,0 +1,22 @@
+package sk.upjs.ics.presentr2017;
+
+class User {
+    private String login;
+
+    public User(String login) {
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public String toString() {
+        return this.login;
+    }
+}
