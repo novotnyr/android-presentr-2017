65b8841d96ba809ed56ac8235f9326f9deaf8898
Robert Novotny <robert.novotny@dcoresystems.com>
2017-04-02 14:20:47 +0200

    Pridany AsyncTask pre registraciu

---
+++ app/src/main/java/sk/upjs/ics/presentr2017/IAmHereAsyncTask.java
@@ -0,0 +1,31 @@
+package sk.upjs.ics.presentr2017;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+public class IAmHereAsyncTask extends AsyncTask<String, Void, Boolean> {
+    public static final String TAG = IAmHereAsyncTask.class.getCanonicalName();
+
+    @Override
+    protected Boolean doInBackground(String... userNames) {
+        try {
+            if (userNames == null || userNames.length != 1) {
+                throw new IllegalArgumentException("A single user name must be provided");
+            }
+            String userName = userNames[0];
+
+            Api api = RetrofitFactory.getApi();
+            Call<Void> call = api.register(userName);
+            int code = call.execute().code();
+            return code == 200;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+}
---
+++ app/src/main/java/sk/upjs/ics/presentr2017/RetrofitFactory.java
@@ -0,0 +1,19 @@
+package sk.upjs.ics.presentr2017;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+public class RetrofitFactory {
+    public static Retrofit getRetrofit() {
+        String baseUrl = "http://ics.upjs.sk/~novotnyr/android/demo/presentr/index.php/available-users";
+
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+    }
+
+    public static Api getApi() {
+        return getRetrofit().create(Api.class);
+    }
+}
