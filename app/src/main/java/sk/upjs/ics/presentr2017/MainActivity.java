45d7cb356c7dab7854b5cb31c5c748bdfb1d08c9
Robert Novotny <robert.novotny@dcoresystems.com>
2017-04-02 15:09:28 +0200

    Implementovany asynchronny loading pouzivatelov

--- app/src/main/java/sk/upjs/ics/presentr2017/MainActivity.java
+++ app/src/main/java/sk/upjs/ics/presentr2017/MainActivity.java
@@ -1,5 +1,7 @@
 package sk.upjs.ics.presentr2017;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,9 +9,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<User>> {
     private ListView userListView;
 
     private ArrayAdapter<User> userListViewAdapter;
@@ -19,15 +22,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<User> users = Arrays.asList(new User("john"), new User("suzanne"), new User("ringo"), new User("helen"));
-        this.userListViewAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, users);
+        this.userListViewAdapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_1);
 
         this.userListView = (ListView) findViewById(R.id.userListView);
         this.userListView.setAdapter(this.userListViewAdapter);
+
+        getLoaderManager().restartLoader(0, Bundle.EMPTY, this);
     }
 
     public void onFloatingActionButtonClick(View view) {
         String userName = "android";
         new IAmHereAsyncTask().execute(userName);
+    }
+
+    @Override
+    public Loader<List<User>> onCreateLoader(int id, Bundle args) {
+        return new UserListLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<User>> loader, List<User> users) {
+        this.userListViewAdapter.clear();
+        this.userListViewAdapter.addAll(users);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<User>> loader) {
+        this.userListViewAdapter.clear();
     }
 }
