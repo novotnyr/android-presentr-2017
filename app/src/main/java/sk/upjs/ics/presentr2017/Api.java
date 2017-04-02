88f52693958ed26f61cde43f48f99af91e0d5142
Robert Novotny <robert.novotny@dcoresystems.com>
2017-04-02 14:20:33 +0200

    Uprava Retrofit API, aby bral pouzivatela v parametri

--- app/src/main/java/sk/upjs/ics/presentr2017/Api.java
+++ app/src/main/java/sk/upjs/ics/presentr2017/Api.java
@@ -5,10 +5,11 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface Api {
-    @POST("available-users")
-    Call<Void> register();
+    @POST("available-users/{userName}")
+    Call<Void> register(@Path("username") String username);
 
     @GET("available-users")
     Call<List<User>> getUsers();
